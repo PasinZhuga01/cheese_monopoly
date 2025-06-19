2f00e31349a0dd8f58c235bc53a015537323ccfe
PasinZhuga01 <sergijumanskij@gmail.com>
2025-06-20 00:19:15 +0300

    feat(content_box): added height, change styles

--- src/app/common/components/content-box/content-box.ts
+++ src/app/common/components/content-box/content-box.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-content-box',
-	imports: [],
+	imports: [CommonModule],
 	templateUrl: './content-box.html',
 	styleUrl: './content-box.css'
 })
-export class ContentBox {}
+export class ContentBox {
+	@Input() public height: number | null = null;
+
+	protected _getStyles(): { height: string } {
+		return { height: this.height ? this.height + 'vh' : 'auto' };
+	}
+}
