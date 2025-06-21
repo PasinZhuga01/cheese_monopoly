6969a669610dadce57ff09025fe55ac08c1dc0d6
PasinZhuga01 <sergijumanskij@gmail.com>
2025-06-22 02:19:08 +0300

    feat(content_section): added content_section

--- src/app/common/layout/layout-module.ts
+++ src/app/common/layout/layout-module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { ContentBox } from './components/content-box/content-box';
+import { ContentSection } from './components/content-section/content-section';
 
 @NgModule({
 	declarations: [],
-	imports: [Header, Footer, ContentBox],
-	exports: [Header, Footer, ContentBox]
+	imports: [Header, Footer, ContentBox, ContentSection],
+	exports: [Header, Footer, ContentBox, ContentSection]
 })
 export class LayoutModule {}
