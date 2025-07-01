ab2f657b174161a85ec7cfaf9eadfec351b074f2
PasinZhuga01 <sergijumanskij@gmail.com>
2025-07-02 01:00:36 +0300

    feat(auth): added auth-content

--- src/app/features/auth/auth-module.ts
+++ src/app/features/auth/auth-module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule as AngularCommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing-module';
 import { AuthPage } from './components/auth-page/auth-page';
 
+import { CommonModule } from '../../common/common-module';
+import { AuthContent } from './components/auth-content/auth-content';
+
 @NgModule({
-	declarations: [AuthPage],
-	imports: [CommonModule, AuthRoutingModule]
+	declarations: [AuthPage, AuthContent],
+	imports: [AngularCommonModule, AuthRoutingModule, CommonModule]
 })
 export class AuthModule {}
---
+++ src/app/features/auth/components/auth-content/auth-content.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-auth-content',
+	standalone: false,
+	templateUrl: './auth-content.html',
+	styleUrl: './auth-content.css'
+})
+export class AuthContent {}
