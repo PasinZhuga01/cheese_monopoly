d4d69aa1cfe7fe8f1f4732e7d4173aae7393ea73
PasinZhuga01 <sergijumanskij@gmail.com>
2025-07-02 00:53:51 +0300

    feat, fix(controls): added types for text-control and styles. Fix maxLength

--- src/app/common/controls/components/text-control/text-control.ts
+++ src/app/common/controls/components/text-control/text-control.ts
@@ -7,6 +7,8 @@ import { Component, Input } from '@angular/core';
 	styleUrl: './text-control.css'
 })
 export class TextControl {
+	@Input({ required: true }) public type: 'text' | 'password' | 'email' = 'text';
+
 	@Input() public placeholder: string = '';
-	@Input() public maxLength: number = Infinity;
+	@Input() public maxLength: number = 1000;
 }
