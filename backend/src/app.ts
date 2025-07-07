82646b4864bb4c6449c066a380b5501a0ae979be
PasinZhuga01 <sergijumanskij@gmail.com>
2025-07-07 05:33:16 +0300

    feat(api): enable CORS between client and server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors({ origin: process.env['FRONTEND_URL'] }));
 
 export default app;
