677e42b8229b2b098b4f17f37ff128ba9305a6be
PasinZhuga01 <sergijumanskij@gmail.com>
2025-07-17 02:20:41 +0300

    fix: add field "name" to sql table "users"

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     current_player_id INT,
     login VARCHAR(80) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     photo TEXT
 );
 
